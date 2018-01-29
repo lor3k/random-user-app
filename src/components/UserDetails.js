a18b5f34e9b61a3073651244b6590fe038957651
Paweł Lorencowicz <p.lorencowicz@gmail.com>
2018-01-29 15:38:07 +0100

    destr. user

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -5,13 +5,19 @@ const UserDetails = props => {
     const users = props.usersData || []
     const currentEmail = props.match.params.email
     const thisUser = users.find(user => user.email === currentEmail)
+
+    const {
+        name,
+        picture
+    } = thisUser
+
     return (
         <div>
             {thisUser ?
                 <div>
-                    <h1>{thisUser.name.first} {thisUser.name.last}</h1>
+                    <h1>{name.first} {name.last}</h1>
                     <h4>{currentEmail}</h4>
-                    <img src={thisUser.picture.large} />
+                    <img src={picture.large} />
                 </div>
                 : 'User does not exist'}
         </div>
